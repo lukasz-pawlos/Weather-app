56ae3c08bec7b51c4820ed7744dc023f91d082cd
lukasz-pawlos <252925@student.pwr.edu.pl>
2022-04-22 13:52:24 +0200

    final version V2

--- pogoda/Form1.Designer.cs
+++ pogoda/Form1.Designer.cs
@@ -38,13 +38,19 @@
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.richTextBox4 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.City = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(626, 36);
+            this.button1.Location = new System.Drawing.Point(646, 255);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 49);
             this.button1.TabIndex = 0;
@@ -91,7 +97,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(549, 410);
+            this.textBox1.Location = new System.Drawing.Point(569, 479);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(255, 40);
@@ -112,11 +118,11 @@
             this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.richTextBox3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.richTextBox3.Location = new System.Drawing.Point(293, 353);
+            this.richTextBox3.Location = new System.Drawing.Point(272, 353);
             this.richTextBox3.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.ReadOnly = true;
-            this.richTextBox3.Size = new System.Drawing.Size(191, 109);
+            this.richTextBox3.Size = new System.Drawing.Size(282, 162);
             this.richTextBox3.TabIndex = 7;
             this.richTextBox3.Text = "";
             // 
@@ -139,12 +145,70 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.label1.Location = new System.Drawing.Point(583, 353);
+            this.label1.Location = new System.Drawing.Point(583, 433);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(241, 29);
             this.label1.TabIndex = 9;
             this.label1.Text = "Podaj nazwe miasta";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(729, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 68);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Add to favourites";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.City,
+            this.Id});
+            this.dataGridView1.Location = new System.Drawing.Point(605, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(182, 190);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+            // 
+            // City
+            // 
+            this.City.DataPropertyName = "city";
+            this.City.HeaderText = "City";
+            this.City.MinimumWidth = 6;
+            this.City.Name = "City";
+            this.City.ReadOnly = true;
+            this.City.Width = 125;
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "id";
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Visible = false;
+            this.Id.Width = 125;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(528, 343);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 68);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Remove from favourites";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -152,6 +216,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(897, 546);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox4);
             this.Controls.Add(this.richTextBox3);
@@ -164,6 +231,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,6 +248,11 @@
         private System.Windows.Forms.RichTextBox richTextBox3;
         private System.Windows.Forms.RichTextBox richTextBox4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn City;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- pogoda/Form1.cs
+++ pogoda/Form1.cs
@@ -5,18 +5,18 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace pogoda
-{
+{   
     public partial class Form1 : Form
-    {   
+    {
+        favourites favourite = new favourites();
         const string API_KEY = "9152c2ba1c9b3c8546f73355913d6a47";
         public Form1()
         {
             InitializeComponent();
+            showWeatherDataGridView1();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -50,8 +50,10 @@ namespace pogoda
             }
             catch(WebException ex)
             {
-                richTextBox1.Text = "Error: City not found";
+                richTextBox1.Text = "";
                 richTextBox2.Text = "Error: City not found";
+                richTextBox3.Text = "";
+                richTextBox4.Text = "";
                 pictureBox1.ImageLocation = "";
             }
         }
@@ -79,6 +81,75 @@ namespace pogoda
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String city = textBox1.Text.ToString();
+                Api api = new Api();
+                Root weather = api.LoadData(city, API_KEY);
+
+
+                favourite.city = textBox1.Text;
+                using (DBEntities db = new DBEntities())
+                {
+                    db.favourites.Add(favourite);
+                    db.SaveChanges();
+                }
+                showWeatherDataGridView1();
+            }
+            catch (WebException ex)
+            {
+                richTextBox1.Text = "";
+                richTextBox2.Text = "Error: City not found";
+                richTextBox3.Text = "";
+                richTextBox4.Text = "";
+                pictureBox1.ImageLocation = "";
+            }
+
+        }
+
+        void showWeatherDataGridView1()
+        {
+            using (DBEntities db = new DBEntities())
+            {
+                dataGridView1.DataSource = db.favourites.ToList<favourites>();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (DBEntities db = new DBEntities())
+            {
+                var entry = db.Entry(favourite);
+                if(entry.State == System.Data.Entity.EntityState.Detached)
+                {
+                    db.favourites.Attach(favourite);
+                }
+                db.favourites.Remove(favourite);
+                db.SaveChanges();
+                showWeatherDataGridView1();
+            }
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (dataGridView1.CurrentRow.Index != -1)
+            {
+                favourite.id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["id"].Value);
+                using (DBEntities db = new DBEntities())
+                {
+                    favourite = db.favourites.Where(x => x.id == favourite.id).FirstOrDefault();
+                    textBox1.Text = favourite.city;
+                }
+            }
         }
     }
 }
