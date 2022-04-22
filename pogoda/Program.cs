c0561d8523d153dc8c56b26f8036f4c104b02ac1
lukasz-pawlos <252925@student.pwr.edu.pl>
2022-04-22 13:29:17 +0200

    final version

---
+++ pogoda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace pogoda
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+    }
+}
